7d24259679f0a36a5dd0503a005af3fe66fcab9d
zhousg <zhoushugang@itcast.cn>
2019-04-20 17:38:02 +0800

    拦截请求加token&拦截响应判断token失效

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,8 +1,29 @@
 <template>
   <el-container class="home_container">
-    <el-header class="home_header">Header</el-header>
+    <el-header class="home_header">
+      <el-button icon="iconfont icon-menu" @click="toggleMenu()" size="mini" circle></el-button>
+      <span class="title">品优购后台管理系统</span>
+      <el-button class="logout" type="danger" size="mini" round>退出</el-button>
+    </el-header>
     <el-container>
-      <el-aside class="home_aside" width="200px">Aside</el-aside>
+      <el-aside class="home_aside" :width="collapse?'65px':'180px'">
+        <el-menu
+          :collapse="collapse"
+          :collapse-transition="false"
+          style="border: none; margin-top: 5px"
+          background-color="#333744"
+          text-color="#fff"
+          active-text-color="#ffd04b">
+          <el-submenu index="1">
+            <template slot="title">
+              <i class="el-icon-location"></i>
+              <span>导航一</span>
+            </template>
+            <el-menu-item index="1-1">选项2</el-menu-item>
+            <el-menu-item index="1-2">选项2</el-menu-item>
+          </el-submenu>
+        </el-menu>
+      </el-aside>
       <el-main class="home_main">Main</el-main>
     </el-container>
   </el-container>
@@ -10,7 +31,26 @@
 
 <script>
 export default {
-  name: 'Home'
+  name: 'Home',
+  data () {
+    return {
+      collapse: false,
+      menus: []
+    }
+  },
+  mounted () {
+    // 获取菜单数据
+    this.getData()
+  },
+  methods: {
+    toggleMenu () {
+      this.collapse = !this.collapse
+    },
+    getData () {
+      // 获取数据
+      this.$http.get('menus').then(res => console.log(res.data))
+    }
+  }
 }
 </script>
 
@@ -20,6 +60,7 @@ export default {
 }
 .home_header{
   background: #373D41;
+  line-height: 60px;
 }
 .home_aside{
   background: #333744;
@@ -27,4 +68,13 @@ export default {
 .home_main{
   background: #EAEDF1;
 }
+  .title{
+    color: #ccc;
+    font-size: 18px;
+    padding-left: 20px;
+  }
+  .logout{
+    float: right;
+    margin-top: 15px;
+  }
 </style>
--- src/http/index.js
+++ src/http/index.js
@@ -4,6 +4,21 @@ import axios from 'axios'
 // 全局使用 必须依赖  Vue  在main.js中使用
 // baseURL 配置
 axios.defaults.baseURL = 'http://localhost:8888/api/private/v1'
+// 拦截器
+axios.interceptors.request.use(config => {
+  // 给config添加新的信息
+  // config可以给请求头追加属性
+  config.headers.Authorization = sessionStorage.getItem('token')
+  return config
+})
+axios.interceptors.response.use(res => {
+  // 判断 token 是否失效
+  if (res.data.meta.status === 401) {
+    location.href = '#/login'
+  } else {
+    return res
+  }
+})
 
 // 导出
 export default axios
