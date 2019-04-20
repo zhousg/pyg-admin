2d66e460783d8bb0f63bea9184dc09f301c206dc
zhousg <zhoushugang@itcast.cn>
2019-04-20 10:23:41 +0800

    登录框&引入element-ui

--- src/components/Login.vue
+++ src/components/Login.vue
@@ -1,6 +1,9 @@
 <template>
-  <div>
-    <h1>登录</h1>
+  <div class="login_container">
+    <div class="box">
+      <img src="../assets/images/logo.png" alt="">
+      <el-button type="primary">主要按钮</el-button>
+    </div>
   </div>
 </template>
 
@@ -11,5 +14,26 @@ export default {
 </script>
 
 <style scoped>
+  .login_container {
+    width: 100%;
+    height: 100%;
+    background: linear-gradient(45deg, #ccc, #105763);
+  }
 
+  .login_container .box {
+    width: 400px;
+    height: 250px;
+    box-shadow: 0 0 10px #eee;
+    position: absolute;
+    left: 50%;
+    top: 50%;
+    transform: translate(-50%, -60%);
+    border-radius: 4px;
+    background: linear-gradient(45deg, #D2ECF1, #efefef);
+  }
+  .login_container .box img{
+    width: 200px;
+    display: block;
+    margin: 15px auto;
+  }
 </style>
--- src/main.js
+++ src/main.js
@@ -2,8 +2,17 @@ import Vue from 'vue'
 import App from './App'
 import router from './router'
 
-Vue.config.productionTip = false
+/* 依赖全局样式文件 */
+import './assets/css/global.css'
 
+/* 依赖element-ui */
+import ElementUI from 'element-ui'
+import 'element-ui/lib/theme-chalk/index.css'
+Vue.use(ElementUI)
+
+/* 控制台的日志更详细一些 true  */
+Vue.config.productionTip = false
+/* 入口文件作用：导入其他依赖 */
 /* eslint-disable no-new */
 new Vue({
   el: '#app',
