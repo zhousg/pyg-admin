e458dae7c48c5f1644e83016225c04beff79410f
zhousg <zhoushugang@itcast.cn>
2019-04-22 12:09:02 +0800

    用户列表界面绘制

--- src/components/Users.vue
+++ src/components/Users.vue
@@ -1,12 +1,71 @@
 <template>
   <div class="users_container">
-    用户列表
+    <el-breadcrumb separator="/">
+      <el-breadcrumb-item :to="{ path: '/' }">首页</el-breadcrumb-item>
+      <el-breadcrumb-item>用户管理</el-breadcrumb-item>
+      <el-breadcrumb-item>用户列表</el-breadcrumb-item>
+    </el-breadcrumb>
+    <el-card>
+      <!-- 搜索框 和 添加按钮 镂空 -->
+      <!-- 槽宽  :gutter="20"  :span="6" 多少份  默认24份-->
+      <el-row :gutter="20">
+        <el-col :span="6">
+            <el-input placeholder="请输入内容">
+              <el-button slot="append" icon="el-icon-search"></el-button>
+            </el-input>
+        </el-col>
+        <el-col :span="18">
+          <el-button type="primary" plain>添加用户</el-button>
+        </el-col>
+      </el-row>
+      <!--表格-->
+      <el-table
+        :data="tableData"
+        stripe
+        style="width: 100%">
+        <el-table-column
+          prop="date"
+          label="日期"
+          width="180">
+        </el-table-column>
+        <el-table-column
+          prop="name"
+          label="姓名"
+          width="180">
+        </el-table-column>
+        <el-table-column
+          prop="address"
+          label="地址">
+        </el-table-column>
+      </el-table>
+    </el-card>
   </div>
 </template>
 
 <script>
 export default {
-  name: 'Users'
+  name: 'Users',
+  data () {
+    return {
+      tableData: [{
+        date: '2016-05-02',
+        name: '王小虎',
+        address: '上海市普陀区金沙江路 1518 弄'
+      }, {
+        date: '2016-05-04',
+        name: '王小虎',
+        address: '上海市普陀区金沙江路 1517 弄'
+      }, {
+        date: '2016-05-01',
+        name: '王小虎',
+        address: '上海市普陀区金沙江路 1519 弄'
+      }, {
+        date: '2016-05-03',
+        name: '王小虎',
+        address: '上海市普陀区金沙江路 1516 弄'
+      }]
+    }
+  }
 }
 </script>
 
