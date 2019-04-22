b2714d72cfcdd639b647df0fb62664b7a96984f8
zhousg <zhoushugang@itcast.cn>
2019-04-22 15:43:38 +0800

    列表渲染&分页实现

--- src/components/Users.vue
+++ src/components/Users.vue
@@ -20,24 +20,43 @@
       </el-row>
       <!--表格-->
       <el-table
-        :data="tableData"
+        :data="userList"
         stripe
         style="width: 100%">
-        <el-table-column
-          prop="date"
-          label="日期"
-          width="180">
+        <el-table-column prop="username" label="用户名"></el-table-column>
+        <el-table-column prop="email" label="邮箱"></el-table-column>
+        <el-table-column prop="mobile" label="电话"></el-table-column>
+        <el-table-column prop="role_name" label="角色"></el-table-column>
+        <el-table-column prop="mg_state" label="状态">
+          <template slot-scope="scope">
+            <!-- 使用当前行的数据  scope.row  状态 scope.row.mg_state -->
+            <!-- el-switch 默认使用布尔值  如果为true激活 反之...  -->
+            <!-- active-color="#13ce66" 激活   inactive-color="#ff4949" 没激活颜色 -->
+            <el-switch
+              v-model="scope.row.mg_state"
+              active-color="#13ce66"
+              inactive-color="#ccc">
+            </el-switch>
+          </template>
         </el-table-column>
-        <el-table-column
-          prop="name"
-          label="姓名"
-          width="180">
-        </el-table-column>
-        <el-table-column
-          prop="address"
-          label="地址">
+        <el-table-column label="操作">
+          <el-button-group>
+            <el-button icon="el-icon-edit" round></el-button>
+            <el-button icon="el-icon-delete" round></el-button>
+            <el-button icon="el-icon-setting" round></el-button>
+          </el-button-group>
         </el-table-column>
       </el-table>
+      <!--分页-->
+      <div class="pager_container">
+        <el-pagination
+          @current-change="changePager"
+          :page-size="reqParams.pagesize"
+          background
+          layout="prev, pager, next"
+          :total="total">
+        </el-pagination>
+      </div>
     </el-card>
   </div>
 </template>
@@ -47,28 +66,43 @@ export default {
   name: 'Users',
   data () {
     return {
-      tableData: [{
-        date: '2016-05-02',
-        name: '王小虎',
-        address: '上海市普陀区金沙江路 1518 弄'
-      }, {
-        date: '2016-05-04',
-        name: '王小虎',
-        address: '上海市普陀区金沙江路 1517 弄'
-      }, {
-        date: '2016-05-01',
-        name: '王小虎',
-        address: '上海市普陀区金沙江路 1519 弄'
-      }, {
-        date: '2016-05-03',
-        name: '王小虎',
-        address: '上海市普陀区金沙江路 1516 弄'
-      }]
+      // 用户列表
+      userList: [],
+      // 传参
+      reqParams: {
+        query: '',
+        pagenum: 1,
+        pagesize: 2
+      },
+      // 总条数
+      total: 0
+    }
+  },
+  mounted () {
+    // 用户列表数据获取
+    this.getData()
+  },
+  methods: {
+    async getData () {
+      // 1. get请求  参数本来url?后面
+      // 2. 联想post传参  post(url,{参数})
+      // 3. get传参  get(url,{params:{参数}})
+      const {data: {data, meta}} = await this.$http.get('users', {params: this.reqParams})
+      if (meta.status !== 200) return this.$message.error('获取用户属性失败')
+      // 列表数据
+      this.userList = data.users
+      // 总条数
+      this.total = data.total
+    },
+    changePager (newPage) {
+      // 进行分页查询  需求：当前页码
+      // 获取数据 前 要使用当前页码
+      this.reqParams.pagenum = newPage
+      this.getData()
     }
   }
 }
 </script>
 
 <style scoped>
-
 </style>
