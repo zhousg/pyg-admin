33e1a2bc29dbfac94922bdfb6ecdccaede258d38
zhousg <zhoushugang@itcast.cn>
2019-04-23 17:53:36 +0800

    编辑角色删除角色删除权限

--- src/components/auth/Roles-Mixin.js
+++ src/components/auth/Roles-Mixin.js
@@ -16,6 +16,60 @@ export default {
         roleDesc: [
           {required: true, message: '角色描述必填', trigger: 'blur'}
         ]
+      },
+      /* 编辑相关的数据 */
+      editDialogFormVisible: false,
+      editForm: {},
+      editRules: {
+        roleName: [
+          {required: true, message: '角色名称必填', trigger: 'blur'}
+        ],
+        roleDesc: [
+          {required: true, message: '角色描述必填', trigger: 'blur'}
+        ]
+      },
+      /* 分配权限相关的数据 */
+      rightDialogFormVisible: false,
+      rightTree: [{
+        id: 1,
+        label: '一级 1',
+        children: [{
+          id: 4,
+          label: '二级 1-1',
+          children: [{
+            id: 9,
+            label: '三级 1-1-1'
+          }, {
+            id: 10,
+            label: '三级 1-1-2'
+          }]
+        }]
+      }, {
+        id: 2,
+        label: '一级 2',
+        children: [{
+          id: 5,
+          label: '二级 2-1'
+        }, {
+          id: 6,
+          label: '二级 2-2'
+        }]
+      }, {
+        id: 3,
+        label: '一级 3',
+        children: [{
+          id: 7,
+          label: '二级 3-1'
+        }, {
+          id: 8,
+          label: '二级 3-2'
+        }]
+      }],
+      defaultProps: {
+        // 数据结构中 下一级数据的字段名称
+        children: 'children',
+        // 显示的文字  的数据字段的名称
+        label: 'label'
       }
     }
   },
@@ -23,6 +77,84 @@ export default {
     this.getData()
   },
   methods: {
+    // 显示分配权限的对话框
+    showRightDialog () {
+      this.rightDialogFormVisible = true
+    },
+    // 分配权限
+    rightSubmit () {
+    },
+    // 删除权限
+    delRights (row, rightId) {
+      this.$confirm('是否删除该权限?', '温馨提示', {
+        confirmButtonText: '确定',
+        cancelButtonText: '取消',
+        type: 'warning'
+      }).then(async () => {
+        const {data: {data, meta}} = await this.$http.delete(`roles/${row.id}/rights/${rightId}`)
+        if (meta.status !== 200) return this.$message.error('删除失败')
+        this.$message.success('删除成功')
+        // 更新整个列表  重新展开后  看到操作结果
+        // this.getData()
+        // 局部更新 当前行的数据  child 数据
+        // 当前修改后返回的数据 就是child数据
+        // 处理成需要的数据结构
+        data.forEach(item => {
+          item.child = item.children
+          delete item.children
+          item.child.forEach(item => {
+            item.child = item.children
+            delete item.children
+          })
+        })
+        row.child = data
+      }).catch(() => {})
+    },
+    // 显示编辑对话框
+    showEditDialog (role) {
+      this.editDialogFormVisible = true
+      // 填充默认数据
+      this.$nextTick(async () => {
+        this.$refs.editForm.resetFields()
+        // 获取 填充  问题：重置表单的数据  会 清除row的数据
+        // this.editForm = role
+        // 使用ajax的数据
+        const {data: {data, meta}} = await this.$http.get(`roles/${role.id}`)
+        if (meta.status !== 200) return this.$message.error('获取角色失败')
+        this.editForm = data
+      })
+    },
+    // 提交编辑
+    editSubmit () {
+      this.$refs.editForm.validate(async valid => {
+        if (valid) {
+          // 注意ID是 roleId
+          const {data: {meta}} = await this.$http.put(`roles/${this.editForm.roleId}`, {
+            roleName: this.editForm.roleName,
+            roleDesc: this.editForm.roleDesc
+          })
+          if (meta.status !== 200) return this.$message.error('编辑角色失败')
+          this.$message.success('编辑角色成功')
+          // 关闭编辑对话框  更新列表
+          this.editDialogFormVisible = false
+          this.getData()
+        }
+      })
+    },
+    // 删除
+    delRoles (id) {
+      this.$confirm('是否删除该数据?', '温馨提示', {
+        confirmButtonText: '确定',
+        cancelButtonText: '取消',
+        type: 'warning'
+      }).then(async () => {
+        // 点击了确认  发请求
+        const {data: {meta}} = await this.$http.delete(`roles/${id}`)
+        if (meta.status !== 200) return this.$message.error('删除失败')
+        this.$message.success('删除成功')
+        this.getData()
+      }).catch(() => {})
+    },
     // 显示添加对话框
     showAddDialog () {
       this.addDialogFormVisible = true
--- src/components/auth/Roles.vue
+++ src/components/auth/Roles.vue
@@ -17,19 +17,19 @@
                     v-for="(item,i) in scope.row.child"
                     :key="item.id">
               <el-col :span="4">
-                <el-tag closable>{{item.authName}}</el-tag>
+                <el-tag @close="delRights(scope.row,item.id)" closable>{{item.authName}}</el-tag>
                 <span class="el-icon-caret-right"></span>
               </el-col>
               <el-col :span="20">
                 <!--二级权限-->
                 <el-row :style="{'border-top':i===0?'none':'1px solid #eee'}" v-for="(secondItem,i) in item.child" :key="secondItem.id">
                   <el-col :span="8">
-                    <el-tag closable type="success">{{secondItem.authName}}</el-tag>
+                    <el-tag @close="delRights(scope.row,secondItem.id)" closable type="success">{{secondItem.authName}}</el-tag>
                     <span class="el-icon-caret-right"></span>
                   </el-col>
                   <el-col :span="16">
                     <!--三级权限-->
-                    <el-tag v-for="lastItem in secondItem.child" :key="lastItem.id" closable type="warning">{{lastItem.authName}}</el-tag>
+                    <el-tag @close="delRights(scope.row,lastItem.id)" v-for="lastItem in secondItem.child" :key="lastItem.id" closable type="warning">{{lastItem.authName}}</el-tag>
                   </el-col>
                 </el-row>
               </el-col>
@@ -42,9 +42,9 @@
         <el-table-column label="操作">
           <template slot-scope="scope">
             <el-button-group>
-              <el-button icon="el-icon-edit"  round></el-button>
-              <el-button icon="el-icon-delete"  round></el-button>
-              <el-button icon="el-icon-setting" round></el-button>
+              <el-button icon="el-icon-edit" @click="showEditDialog(scope.row)" round></el-button>
+              <el-button icon="el-icon-delete" @click="delRoles(scope.row.id)"  round></el-button>
+              <el-button icon="el-icon-setting" @click="showRightDialog()" round></el-button>
             </el-button-group>
           </template>
         </el-table-column>
@@ -63,6 +63,35 @@
       <div slot="footer" class="dialog-footer">
         <el-button @click="addDialogFormVisible = false">取 消</el-button>
         <el-button type="primary" @click="addSubmit()">确 定</el-button>
+      </div>
+    </el-dialog>
+    <!--编辑的对话框-->
+    <el-dialog width="400px" title="编辑角色" :visible.sync="editDialogFormVisible">
+      <el-form ref="editForm" :model="editForm" :rules="editRules" label-width="80px">
+        <el-form-item label="角色名称" prop="roleName">
+          <el-input v-model="editForm.roleName"></el-input>
+        </el-form-item>
+        <el-form-item label="角色描述" prop="roleDesc">
+          <el-input v-model="editForm.roleDesc"></el-input>
+        </el-form-item>
+      </el-form>
+      <div slot="footer" class="dialog-footer">
+        <el-button @click="editDialogFormVisible = false">取 消</el-button>
+        <el-button type="primary" @click="editSubmit()">确 定</el-button>
+      </div>
+    </el-dialog>
+    <!--分配权限的对话框-->
+    <el-dialog width="400px" title="分配权限" :visible.sync="rightDialogFormVisible">
+      <el-tree
+        :data="rightTree"
+        show-checkbox
+        node-key="id"
+        :default-expand-all="true"
+        :props="defaultProps">
+      </el-tree>
+      <div slot="footer" class="dialog-footer">
+        <el-button @click="rightDialogFormVisible = false">取 消</el-button>
+        <el-button type="primary" @click="rightSubmit()">确 定</el-button>
       </div>
     </el-dialog>
   </div>
